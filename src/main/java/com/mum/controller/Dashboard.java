89708f36c113c716896e61251a9d887d12c29b28
Festus Iipito <dev3320770@gmail.com>
2019-03-19 15:10:16 -0500

    none

--- src/main/java/com/mum/controller/Dashboard.java
+++ src/main/java/com/mum/controller/Dashboard.java
@@ -2,20 +2,30 @@ package com.mum.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mum.model.User;
+import com.mum.service.UserService;
+
 @Controller
 public class Dashboard {
-	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping(value= {"/","/dashboard"}, method=RequestMethod.GET)
-	public String UserDashboard(Principal principal) {
-	
+	public String UserDashboard(Principal principal, Model model) {
+		
+		User user = userService.findUserByEmail(principal.getName());
+		System.out.println(user.getFirstName());
+		model.addAttribute("user" , user);
+		
 		String role=  ((Authentication) principal).getAuthorities().toString();
+		
 		System.out.println(principal);
 		
 		if(role.equals("[ADMIN]")) {
