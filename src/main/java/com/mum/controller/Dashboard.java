85946d66e1a8faedc2d544e2a3718fa62b2b3285
Festus Iipito <dev3320770@gmail.com>
2019-03-19 00:31:32 -0500

    Dashboard

--- src/main/java/com/mum/controller/Dashboard.java
+++ src/main/java/com/mum/controller/Dashboard.java
@@ -1,5 +1,9 @@
 package com.mum.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,13 +11,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Dashboard {
 	
-
-	@RequestMapping(value="dashboard", method=RequestMethod.GET)
-	public String UserDashboard() {
+	
+	@RequestMapping(value= {"/","/dashboard"}, method=RequestMethod.GET)
+	public String UserDashboard(Principal principal) {
+	
+		String role=  ((Authentication) principal).getAuthorities().toString();
+		System.out.println(role);
 		
+		if(role.equals("[ADMIN]")) {
+			return "admin/students";
+		}
+		
+		if (role.equals("[FACULTY]")) {
+			return "faculty/dashboard";
+		}
 		
 	
+		
 		return "dashboard";
+	}
+	
+	
+	@RequestMapping(value="/admin/students", method=RequestMethod.GET)
+	public String getStudents() {
+	
+		
+		return "admin/students";
+	}
+	
+	
+	@RequestMapping(value="/admin/entries", method=RequestMethod.GET)
+	public String getEntries() {
+	
+		
+		return "admin/entries";
 	}
 	
 
